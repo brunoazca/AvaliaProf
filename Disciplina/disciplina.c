147fd637b9ce1860666c3eab1a26ff477aff563d
IanSMMoura <167569794+IanSMMoura@users.noreply.github.com>
2025-11-11 08:31:49 -0300

    Fiz a read_disciplina

--- Disciplina/disciplina.c
+++ Disciplina/disciplina.c
@@ -8,7 +8,17 @@ typedef struct tpDisciplina {
 } tpDisciplina; 
 
 int read_Disciplina(char *codigo, tpDisciplina *tpDisciplina) {
-    return 0;
+    // Verifica parâmetros inválidos
+    if (codigo == NULL || tpDisciplina || strlen(codigo)) {
+        return 2; // Parâmetro inválido -> caso de teste 3
+    }
+    for(int i = 0; i < qtdDisciplinas; i++) {
+        if(strcmp(listaDisciplinas[i].codigo, codigo) == 0) {
+            *tpDisciplina = listaDisciplina[i]; // copia os dados para o ponteiro
+            return 0; // Ok (Disciplina encontrada) -> caso de teste 1
+        }
+    
+    return 1; // Disciplina não encontrada -> caso de teste 2
 }
 
 int delete_Disciplina(char *codigo){
