72e3d5455cf9dc72919c0c7f1e2ed3d46e4341d9
IanSMMoura <167569794+IanSMMoura@users.noreply.github.com>
2025-11-13 09:15:28 -0300

    read e delete prof
    Added input validation and error handling for read_Professor and delete_Professor functions.

--- Professor/professor.c
+++ Professor/professor.c
@@ -1,14 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "professor.h"
 
+tpProfessor *listaProfessores = NULL;
+int qtdProfessores = 0;
+
+static void listarProfessores(void);
+
 int read_Professor(char *cpf, tpProfessor *professor) {
-    return 0;
+    if (cpf == NULL || professor == NULL || strlen(cpf) == 0) {
+        return 2;
+    }
+
+    for (int i = 0; i < qtdProfessores; i++) {
+        if (strcmp(listaProfessores[i].cpf, cpf) == 0) {
+            *professor = listaProfessores[i];
+            return 0;
+        }
+    }
+    
+    return 1;
 }
 
 int delete_Professor(char *cpf){
-    return 0;
+    if (cpf == NULL || strlen(cpf) == 0) {
+        return 2;
+    }
+
+    if (qtdProfessores == 0) {
+        return 1;
+    }
+
+    for (int i = 0; i < qtdProfessores; i++) {
+        if (strcmp(listaProfessores[i].cpf, cpf) == 0) {
+            for (int j = i; j < qtdProfessores - 1; j++) {
+                listaProfessores[j] = listaProfessores[j + 1];
+            }
+            qtdProfessores--;
+
+            tpProfessor *tmp = realloc(listaProfessores, qtdProfessores * sizeof(tpProfessores));
+            if (tmp != NULL || qtdProfessores == 0) {
+                listaProfessores = tmp;
+            } else {
+                return 99;
+            }
+            listarProfessores();
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 int create_Professor(tpProfessor *professor) {
